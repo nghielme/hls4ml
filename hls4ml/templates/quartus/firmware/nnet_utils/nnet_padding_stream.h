f358547bac3ed0729e01797a42e177a8cc8e41bd
Jovan Mitrevski <jmitrevs@fnal.gov>
2022-11-08 16:11:15 -0600

    unoptimized 1d zeropad

--- hls4ml/templates/quartus/firmware/nnet_utils/nnet_padding_stream.h
+++ hls4ml/templates/quartus/firmware/nnet_utils/nnet_padding_stream.h
@@ -24,58 +24,86 @@ inline void fill_data(stream<data_T> &data, stream<res_T> &res) {
     res.write(res_part);
 }
 
+// template<class data_T, class res_T, typename CONFIG_T>
+// void zeropad1d_cl(stream<data_T> &data, stream<res_T> &res) {
+//     PadLeft:
+//     for (int i = 0; i < CONFIG_T::pad_left; i++) {
+//         fill_zero<res_T, CONFIG_T>(res);
+//     }
+
+//     CopyMain:
+//     for (int i = 0; i < CONFIG_T::in_width; i++) {
+//         fill_data<data_T, res_T, CONFIG_T>(data, res);
+//     }
+
+//     PadRight:
+//     for (int i = 0; i < CONFIG_T::pad_right; i++) {
+//         fill_zero<res_T, CONFIG_T>(res);
+//     }
+// }
+
+// This is an unoptimal version that doesn't run into the error above
+
 template<class data_T, class res_T, typename CONFIG_T>
 void zeropad1d_cl(stream<data_T> &data, stream<res_T> &res) {
-    PadLeft: 
-    for (int i = 0; i < CONFIG_T::pad_left; i++) {
-        fill_zero<res_T, CONFIG_T>(res);
+
+    res_T res_array[CONFIG_T::out_width];
+
+    ZeroOutputArray:
+    for (int i = 0; i < CONFIG_T::out_width; i++) {
+        for (int j = 0; j < CONFIG_T::n_chan; j++) {
+            res_array[i][j] = 0;
+        }
     }
 
-    CopyMain: 
+    CopyMain:
     for (int i = 0; i < CONFIG_T::in_width; i++) {
-        fill_data<data_T, res_T, CONFIG_T>(data, res);
+        auto dataval = data.read();
+        for (int j = 0; j < CONFIG_T::n_chan; j++) {
+            res_array[i+CONFIG_T::pad_left][j] = dataval[j];
+        }
     }
 
-    PadRight: 
-    for (int i = 0; i < CONFIG_T::pad_right; i++) {
-        fill_zero<res_T, CONFIG_T>(res);
+    StreamOut:
+    for (int i = 0; i < CONFIG_T::out_width; i++) {
+        res.write(res_array[i]);
     }
 }
 
 template<class data_T, class res_T, typename CONFIG_T>
 void zeropad2d_cl(stream<data_T> &data, stream<res_T> &res) {
     #pragma loop_coalesce 2
-    PadTop: 
+    PadTop:
     for (int i = 0; i < CONFIG_T::pad_top; i++) {
-        PadTopWidth: 
+        PadTopWidth:
         for (int j = 0; j < CONFIG_T::out_width; j++) {
             fill_zero<res_T, CONFIG_T>(res);
         }
     }
 
     #pragma loop_coalesce 2
-    PadMain: 
+    PadMain:
     for (int i = 0; i < CONFIG_T::in_height; i++) {
-        
-        PadLeft: 
+
+        PadLeft:
         for (int j = 0; j < CONFIG_T::pad_left; j++) {
             fill_zero<res_T, CONFIG_T>(res);
         }
 
-        CopyMain: 
+        CopyMain:
         for (int j = 0; j < CONFIG_T::in_width; j++) {
             fill_data<data_T, res_T, CONFIG_T>(data, res);
         }
 
-        PadRight: 
+        PadRight:
         for (int j = 0; j < CONFIG_T::pad_right; j++) {
             fill_zero<res_T, CONFIG_T>(res);
         }
     }
 
-    PadBottom: 
+    PadBottom:
     for (int i = 0; i < CONFIG_T::pad_bottom; i++) {
-        PadBottomWidth: 
+        PadBottomWidth:
         for (int j = 0; j < CONFIG_T::out_width; j++) {
             fill_zero<res_T, CONFIG_T>(res);
         }
