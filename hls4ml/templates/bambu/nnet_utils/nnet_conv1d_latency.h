52f2b4ff3772787748a63755e3388ecad9c81f08
buzz <francesco260403@gmail.com>
2026-05-25 12:55:46 +0200

    fixed wrong index

--- hls4ml/templates/bambu/nnet_utils/nnet_conv1d_latency.h
+++ hls4ml/templates/bambu/nnet_utils/nnet_conv1d_latency.h
@@ -80,7 +80,7 @@ PartitionLoop:
             for (int i_res = 0; i_res < mult_n_out; i_res++) {
                 //#pragma HLS UNROLL
                 res[(i_part * CONFIG_T::n_pixels + i_pxl) * mult_n_out + i_res] =
-                    cast<data_T, res_T, typename CONFIG_T::mult_config>(acc[i_pxl][i_res]);
+                  cast<data_T, res_T, typename CONFIG_T::mult_config>(acc[i_res]);
             }
         }
     }
